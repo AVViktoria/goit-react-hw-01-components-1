37e3402716694bb2d9e23175c68500a0451d7ebd
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-10-21 23:32:09 -0700

    statistics emotion styles  done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,28 +1,38 @@
 import { React } from 'react';
 import PropTypes from 'prop-types';
-import css from './Statistics.module.css';
+// import css from './Statistics.module.css';
 // import getRandomHexColor from 'utils/randomColor';
+
 import getRandomHexColor from 'utils';
+
+//*    styles..//
+import {
+  StatisticsContainer,
+  StatisticsTitle,
+  StatisticsList,
+  StatisticsItem,
+  StatisticsLabel,
+  StatisticsPercentage,
+} from './Statistics.styled';
 
 export default function Statistics({ title, stats }) {
   return (
-    <section className={css.statistics}>
-      {title && <h2 className={css.title}>{title}</h2>}
-      <ul className={css.statList}>
+    <StatisticsContainer>
+      {title && <StatisticsTitle>{title}</StatisticsTitle>}
+      <StatisticsList>
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li
+            <StatisticsItem
               key={id}
-              className={css.item}
               style={{ backgroundColor: getRandomHexColor() }}
             >
-              <span className={css.label}>{label}</span>
-              <span className={css.percentage}>{percentage}%</span>
-            </li>
+              <StatisticsLabel>{label}</StatisticsLabel>
+              <StatisticsPercentage>{percentage}%</StatisticsPercentage>
+            </StatisticsItem>
           );
         })}
-      </ul>
-    </section>
+      </StatisticsList>
+    </StatisticsContainer>
   );
 }
 
@@ -35,3 +45,35 @@ Statistics.propTypes = {
     }).isRequired
   ),
 };
+
+// export default function Statistics({ title, stats }) {
+//   return (
+//     <section className={css.statistics}>
+//       {title && <h2 className={css.title}>{title}</h2>}
+//       <ul className={css.statList}>
+//         {stats.map(({ id, label, percentage }) => {
+//           return (
+//             <li
+//               key={id}
+//               className={css.item}
+//               style={{ backgroundColor: getRandomHexColor() }}
+//             >
+//               <span className={css.label}>{label}</span>
+//               <span className={css.percentage}>{percentage}%</span>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </section>
+//   );
+// }
+
+// Statistics.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   stats: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       label: PropTypes.string,
+//       percentage: PropTypes.number,
+//     }).isRequired
+//   ),
+// };
--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,1 +1,78 @@
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
+
+
+export const StatisticsContainer = styled.section`
+  background-color: rgb(226, 226, 226);
+  padding: 10px;
+  width: 400px;
+  border-radius: 10px;
+`;
+
+export const StatisticsTitle = styled.h2`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin-top: 10px;
+   color: rgb(84, 82, 82);
+   margin-bottom: 30px;
+`;
+
+export const StatisticsList = styled.ul`
+ display: flex;
+`;
+
+export const StatisticsItem = styled.li`
+ display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 60px;
+  padding: 20px;
+
+  color: #fff;
+  background-color: #ccc;
+  
+`;
+
+
+export const StatisticsLabel = styled.span`
+
+`;
+
+export const StatisticsPercentage = styled.span`
+
+`;
+
+
+
+
+// .statistics {
+  // background-color: rgb(226, 226, 226);
+  // padding: 10px;
+  // width: 400px;
+  // border-radius: 10px;
+// }
+// .title {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   margin-top: 10px;
+//   color: rgb(84, 82, 82);
+//   margin-bottom: 30px;
+// }
+
+// .statList {
+  // display: flex;
+// }
+
+// .item {
+  // display: flex;
+  // flex-direction: column;
+  // align-items: center;
+
+  // width: 60px;
+  // padding: 20px;
+
+  // color: #fff;
+  // background-color: #ccc;
+// }
