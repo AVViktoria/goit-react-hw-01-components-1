988db8eaefff08c71e906b88f6e08ee85948a3ed
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-10-21 23:10:54 -0700

    Profile emotion styles done

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,26 @@
 import { React } from 'react';
 import PropTypes from 'prop-types';
-import css from './Profile.module.css';
+// import css from './Profile.module.css';
 
-//*  ..img
+//*  ..img  //
 import defaultImage from './default-avatar.jpg';
-//*    icons..
+//*    icons..//
 import { TbAt } from 'react-icons/tb';
 import { iconSize } from 'constance';
+//*    styles..//
+import {
+  ProfileContainer,
+  CardDescription,
+  CardAvatar,
+  CardName,
+  CardTag,
+  CardLocation,
+  StatsContainer,
+  Stats,
+  StatsList,
+  StatsLabel,
+  StatsQuantity,
+} from './Profile.styled';
 
 export default function Profile({
   avatar = defaultImage,
@@ -16,33 +30,33 @@ export default function Profile({
   stats: { followers, views, likes },
 }) {
   return (
-    <div className={css.profile}>
-      <div className={css.description}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>
-          <TbAt className={css.iconEmail} size={iconSize.sm} />
+    <ProfileContainer>
+      <CardDescription>
+        <CardAvatar src={avatar} alt="User avatar" />
+        <CardName>{username}</CardName>
+        <CardTag>
+          <TbAt size={iconSize.sm} />
           {tag}
-        </p>
-        <p className={css.location}>{location}</p>
-      </div>
-      <div className={css.statsContainer}>
-        <ul className={css.stats}>
-          <li className={css.list}>
-            <span className={css.label}>Followers</span>
-            <span className={css.quantity}>{followers}</span>
-          </li>
-          <li className={css.list}>
-            <span className={css.label}>Views</span>
-            <span className={css.quantity}>{views}</span>
-          </li>
-          <li className={css.list}>
-            <span className={css.label}>Likes</span>
-            <span className={css.quantity}>{likes}</span>
-          </li>
-        </ul>
-      </div>
-    </div>
+        </CardTag>
+        <CardLocation>{location}</CardLocation>
+      </CardDescription>
+      <StatsContainer>
+        <Stats>
+          <StatsList>
+            <StatsLabel>Followers</StatsLabel>
+            <StatsQuantity>{followers}</StatsQuantity>
+          </StatsList>
+          <StatsList>
+            <StatsLabel>Views</StatsLabel>
+            <StatsQuantity>{views}</StatsQuantity>
+          </StatsList>
+          <StatsList>
+            <StatsLabel>Likes</StatsLabel>
+            <StatsQuantity>{likes}</StatsQuantity>
+          </StatsList>
+        </Stats>
+      </StatsContainer>
+    </ProfileContainer>
   );
 }
 
@@ -58,3 +72,54 @@ Profile.propTypes = {
     statsLikes: PropTypes.number,
   }),
 };
+
+// export default function Profile({
+//   avatar = defaultImage,
+//   username = 'User Name',
+//   tag,
+//   location,
+//   stats: { followers, views, likes },
+// }) {
+//   return (
+//     <div className={css.profile}>
+//       <div className={css.description}>
+//         <img src={avatar} alt="User avatar" className={css.avatar} />
+//         <p className={css.name}>{username}</p>
+//         <p className={css.tag}>
+//           <TbAt className={css.iconEmail} size={iconSize.sm} />
+//           {tag}
+//         </p>
+//         <p className={css.location}>{location}</p>
+//       </div>
+//       <div className={css.statsContainer}>
+//         <ul className={css.stats}>
+//           <li className={css.list}>
+//             <span className={css.label}>Followers</span>
+//             <span className={css.quantity}>{followers}</span>
+//           </li>
+//           <li className={css.list}>
+//             <span className={css.label}>Views</span>
+//             <span className={css.quantity}>{views}</span>
+//           </li>
+//           <li className={css.list}>
+//             <span className={css.label}>Likes</span>
+//             <span className={css.quantity}>{likes}</span>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// Profile.propTypes = {
+//   username: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+
+//   avatar: PropTypes.string,
+//   stats: PropTypes.shape({
+//     Followers: PropTypes.number,
+//     statsViews: PropTypes.number,
+//     statsLikes: PropTypes.number,
+//   }),
+// };
--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,4 +1,191 @@
 import styled from '@emotion/styled';
 // export const Card = styled.div``;
 
-export const CardContainer = styled.div``;
+export const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 290px;
+  border: 1px solid #e9eef3;
+  background-color: white;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
+    rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+`;
+
+export const CardDescription = styled.div`
+background-position: center;
+background-size: cover;
+`;
+export const CardAvatar = styled.img`
+border: 3px solid #e9eef3;
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  background-position: center;
+  overflow: hidden;
+`;
+
+export const CardName = styled.p`
+align-items: center;
+  font-size: 28px;
+  margin-bottom: 5px;
+  `;
+
+ 
+export const CardTag = styled.p`
+display: flex;
+  align-items: center;
+  align-items: center;
+  font-size: 16px;
+  margin-top: 15px;
+  margin-bottom: 20px;
+  justify-content: center;
+  color: #928a8a;
+  `;
+
+export const CardLocation = styled.p`
+font-size: 20px;
+  margin-bottom: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #928a8a;`;
+
+export const StatsContainer = styled.div`
+background-color: #e9eef3;`;
+
+
+export const Stats = styled.ul` 
+display: grid;
+  flex-direction: row;
+  justify-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  border-top: 1px solid #dddddd;
+`;
+export const StatsList = styled.li`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   padding: 15px;
+   :not(:last-child) {
+   border-right: 1px solid #dddddd;}
+ 
+`;
+export const StatsLabel = styled.span`
+  margin-bottom: 5px;
+  margin-top: 10px;
+  font-size: 15px;
+  font-weight: 700;
+`;
+export const StatsQuantity = styled.span`
+  margin-bottom: 10px;
+  color: #928a8a;
+`;
+
+
+// .profile {
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   width: 290px;
+//   border: 1px solid #e9eef3;
+//   background-color: white;
+//   border-radius: 5px;
+//   overflow: hidden;
+//   box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
+//     rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+//     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+// }
+// .description {
+//   background-position: center;
+//   background-size: cover;
+// }
+
+// .avatar {
+//   border: 3px solid #e9eef3;
+//   border-radius: 50%;
+//   width: 150px;
+//   height: 150px;
+//   margin-top: 30px;
+//   margin-bottom: 30px;
+//   background-position: center;
+//   overflow: hidden;
+// }
+// .name {
+//   align-items: center;
+//   font-size: 28px;
+//   margin-bottom: 5px;
+//   /* font-weight: 700;
+//   margin-right: 15px; */
+// }
+// .tag {
+//   display: flex;
+//   align-items: center;
+//   align-items: center;
+//   font-size: 16px;
+//   margin-top: 15px;
+//   margin-bottom: 20px;
+//   justify-content: center;
+//   color: #928a8a;
+// }
+
+
+// .location {
+//   font-size: 20px;
+//   margin-bottom: 45px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   color: #928a8a;
+// }
+// .statsContainer {
+//   background-color: #e9eef3;
+//   /* width: 100%; */
+// }
+
+// /* .list {
+//   display: grid;
+//   flex-direction: row;
+//   justify-items: center;
+//   /* grid-template-columns: repeat(3, 1fr); */
+// /* } */
+
+// .stats {
+//   /* display: flex;
+//   align-items: center;
+//   justify-content: center; */
+//   display: grid;
+//   flex-direction: row;
+//   justify-items: center;
+//   grid-template-columns: repeat(3, 1fr);
+
+//   border-top: 1px solid #dddddd;
+// }
+
+// .list {
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+
+//   padding: 15px;
+// }
+
+// .list:not(:last-child) {
+//   border-right: 1px solid #dddddd;
+// }
+
+// .label {
+//   margin-bottom: 5px;
+//   margin-top: 10px;
+//   font-size: 15px;
+//   font-weight: 700;
+// }
+// .quantity {
+//   margin-bottom: 10px;
+//   color: #928a8a;
+// }
